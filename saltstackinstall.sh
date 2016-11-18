d67ee9f1b4e32660f18e0cc72264cd5d2a72ec42
Rita Zhang <rita.z.zhang@gmail.com>
2016-11-18 15:26:52 -0800

    Add installation of salt

--- saltstackinstall.sh
+++ saltstackinstall.sh
@@ -2,7 +2,10 @@
 
 echo $(date +"%F %T%z") "starting script saltstackinstall.sh"
 
-curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-yum -y install nodejs
+curl -s -o $HOME/bootstrap_salt.sh -L https://bootstrap.saltstack.com
+sh $HOME/bootstrap_salt.sh -M -p python2-boto 2e8e56c
+easy_install-2.7 pip==7.1.0
+yum install -y gcc gcc-c++ git make libffi-devel openssl-devel python-devel
+pip install -y azure
 
 echo $(date +"%F %T%z") "ending script saltstackinstall.sh"
